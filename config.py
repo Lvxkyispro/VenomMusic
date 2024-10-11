d72272f904c803d5b5cb55c4a50ed441b6189203
Lvxkyispro <158829858+Lvxkyispro@users.noreply.github.com>
2024-10-11 15:34:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ SONG_DOWNLOAD_DURATION = int(
     getenv("SONG_DOWNLOAD_DURATION_LIMIT", "5400")
 )
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", "-1002275427766"))
+LOGGER_ID = int(getenv("LOGGER_ID", "-1002060133086"))
 
 # Get this value from @Hot_Girl_Robot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", "6652287427"))
